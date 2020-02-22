e7f656584b5b638d380d6912a1ce9bbecf639edf
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-22 10:51:26 +0900

    add cache

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,9 @@ def index(request):
     posts = Post.objects.order_by('-published') #asc
 
     #return render(request, 'posts/index.html')
+    # viewsからtemplatesに値を渡したい場合。
+    # データはmodel経由で渡されるので、postモデルをインポートして、それをview経由でtemplateへ渡す
     return render(request, 'posts/index.html', {'posts': posts})
 
-    
+
 # Create your views here.
