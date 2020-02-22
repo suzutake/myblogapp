79024a1eaea9e5e89b2e4c2f851c77443371646a
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-22 11:20:26 +0900

    add summary function in model

--- posts/models.py
+++ posts/models.py
@@ -11,3 +11,6 @@ class Post(models.Model):
     def __str__(self):
         # return post.title for admin.lists
         return self.title
+
+    def summary(self):
+        return self.body[:30]  #pickup first 100 charactor
