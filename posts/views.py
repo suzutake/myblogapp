4b6750ca6cf4392c4849a2df0be5f07d12bbf942
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-22 22:13:40 +0900

    handle 404 error

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -14,8 +14,8 @@ def index(request):
 
 
 def post_detail(request,post_id):
-    post = Post.objects.get(pk=post_id)
-    #print('post_id is ' post_id)
-    #return render(request, 'posts/post_detail.html', {'post_id':post_id})
+    #post = Post.objects.get(pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
+
     return render(request, 'posts/post_detail.html', {'post':post})
 # Create your views here.
