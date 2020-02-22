3de3a42840be6db62b213d30f3d81905d69df934
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-22 10:50:53 +0900

    show data in template, get data from model throw view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
     #return HttpResponse("Hello World!")
-    return render(request, 'posts/index.html')
+
+    posts = Post.objects.order_by('-published') #asc
+
+    #return render(request, 'posts/index.html')
+    return render(request, 'posts/index.html', {'posts': posts})
+
+    
 # Create your views here.
