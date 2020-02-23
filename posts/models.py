09038d9d7a6a1f1f3395d1a686844d284da65b70
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-23 21:41:24 +0900

    add models menumaster and detail

--- posts/models.py
+++ posts/models.py
@@ -14,3 +14,21 @@ class Post(models.Model):
 
     def summary(self):
         return self.body[:45]  #pickup first 100 charactor
+
+
+class MenuMaster(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        # return post.title for admin.lists
+        return self.name
+
+
+class MenuDetail(models.Model):
+    name = models.ForeignKey(MenuMaster, on_delete=models.CASCADE)
+    nailist = models.CharField(max_length=30)
+    money = models.IntegerField(default=0)
+
+    def __str__(self):
+        # return post.title for admin.lists
+        return self.nailist
