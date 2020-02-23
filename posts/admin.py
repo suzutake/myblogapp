419d21b9428071f00eb396e2df90793c4c5c9edb
May Xe <pristi.stanice.hanoi@gmail.com>
2020-02-23 21:48:00 +0900

    confirm adding models

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+# Before register by admin screen, need to Import
+from .models import MenuMaster
+from .models import MenuDetail
+
 admin.site.register(Post)
+
+admin.site.register(MenuMaster)
+admin.site.register(MenuDetail)
